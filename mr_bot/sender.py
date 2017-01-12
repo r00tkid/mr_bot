e08da858d6db1ef27ad959b6d9f5d86b753a5f0f
ccb-590 <born2fish@yandex.ru>
2017-01-12 17:31:21 +0200

    add Sender class for send text, photos, videos, voices and documents

---
+++ mr_bot/sender.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+import traceback
+from mr_logger.logger import Logger
+from telegram.error import BadRequest, Unauthorized
+
+logger = Logger()
+
+
+class Sender:
+    """ this class will send bot messages (photo, voice, video, audio, docs) """
+    """ bot is instance if Telegram bot """
+    """ return True on success and false on error """
+
+    bot = None
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    def send_message(self, chat_id, text=None, photo=None, video=None, document=None,
+                     voice=None, audio=None, reply_markup=None, reply_to_message_id=None):
+        try:
+            if text:
+                self.bot.sendMessage(chat_id=chat_id,
+                                text=text, reply_markup=reply_markup,
+                                reply_to_message_id=reply_to_message_id)
+            elif photo:
+                self.bot.sendPhoto(chat_id=chat_id, photo=photo, reply_markup=reply_markup,
+                                   reply_to_message_id=reply_to_message_id)
+            elif video:
+                self.bot.sendVideo(chat_id=chat_id, video=video, reply_markup=reply_markup,
+                                   reply_to_message_id=reply_to_message_id)
+            elif document:
+                self.bot.sendDocument(chat_id=chat_id, document=document, reply_markup=reply_markup,
+                                   reply_to_message_id=reply_to_message_id)
+            elif voice:
+                self.bot.sendVoice(chat_id=chat_id, voice=voice, reply_markup=reply_markup,
+                                   reply_to_message_id=reply_to_message_id)
+            elif audio:
+                self.bot.sendAudio(chat_id=chat_id, audio=audio, reply_markup=reply_markup,
+                                   reply_to_message_id=reply_to_message_id)
+            return True
+        except BadRequest as bad_request_exception:
+            logger.warn(traceback.format_exception_only(type(bad_request_exception),
+            bad_request_exception))
+        except Unauthorized:
+            logger.warn('cant send message to chat_id %s ( the bot was blocked by the user )'
+            % str(chat_id))
+        except Exception as e:
+            logger.err(e)
+        return False
---
+++ mr_bot/tests/sender_test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+import unittest
+from telegram import Bot
+from sender import Sender, logger
+
+TEST_BOT_TOKEN = '306343134:AAFe0BfoSWR39APT5Aq-SZGiYx5lgAL3fNw'
+TEST_CHAT_ID = 161933860
+bot = Bot(token=TEST_BOT_TOKEN)
+
+
+class SenderTest(unittest.TestCase):
+    sender = Sender(bot=bot)
+
+    def test_send_simple_message(self):
+        result = self.sender.send_message(chat_id=TEST_CHAT_ID,
+                 text='AgADAgADWa0xGyTqpglMQ6fYM8gylh7OgQ0ABOD5lpITdqoF5EQEAAEC')
+        if result:
+            logger.ok('test message was send')
+        else:
+            logger.err('message was not send')
+        self.assertTrue(result)
+
+
+
