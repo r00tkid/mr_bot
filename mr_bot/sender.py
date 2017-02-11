e60bbd39494710c98c808d9ff74b12cb7de66582
ccb-590 <born2fish@yandex.ru>
2017-02-12 01:19:41 +0200

    add parse mode parameter to sender

--- mr_bot/sender.py
+++ mr_bot/sender.py
@@ -19,14 +19,19 @@ class Sender:
         self.bot = bot
 
     # todo venue
-    def send_message(self, chat_id, text=None, photo=None, video=None, document=None,
+    def send_message(self, chat_id, parse_mode=None, text=None, photo=None, video=None, document=None,
                      voice=None, audio=None, sticker=None, location=None, contact=None,
                      reply_markup=None, reply_to_message_id=None):
         try:
             if text:
-                self.bot.sendMessage(chat_id=chat_id,
-                                text=text, reply_markup=reply_markup,
-                                reply_to_message_id=reply_to_message_id)
+                if not parse_mode:
+                    self.bot.sendMessage(chat_id=chat_id,
+                                    text=text, reply_markup=reply_markup,
+                                    reply_to_message_id=reply_to_message_id)
+                else:
+                    self.bot.sendMessage(chat_id=chat_id,
+                                    text=text, reply_markup=reply_markup,
+                                    reply_to_message_id=reply_to_message_id, parse_mode=parse_mode)
             elif photo:
                 self.bot.sendPhoto(chat_id=chat_id, photo=photo, reply_markup=reply_markup,
                                    reply_to_message_id=reply_to_message_id)
