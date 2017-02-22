8133cf72d65eba2db45b260e3d54904870fa5b07
ccb-590 <born2fish@yandex.ru>
2017-02-22 13:14:16 +0200

    Add editMessageText for inline messages

--- mr_bot/sender.py
+++ mr_bot/sender.py
@@ -18,6 +18,21 @@ class Sender:
     def __init__(self, bot):
         self.bot = bot
 
+    def edit_inline_message_text(self, inline_message_id, text, reply_markup):
+        try:
+            self.bot.editMessageText(inline_message_id=inline_message_id,
+                                                            text=text, reply_markup=reply_markup)
+            return True
+        except BadRequest as bad_request_exception:
+            logger.warn(traceback.format_exception_only(type(bad_request_exception),
+            bad_request_exception))
+        except Unauthorized:
+            logger.warn('[Unauthorized]: cant edit message text.\nID=%s' % str(inline_message_id))
+        except Exception as e:
+            logger.err(e)
+        return False
+
+
     # todo venue
     def send_message(self, chat_id, parse_mode=None, text=None, photo=None, video=None, document=None,
                      voice=None, audio=None, sticker=None, location=None, contact=None,
